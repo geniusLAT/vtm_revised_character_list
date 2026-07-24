148a0aaf95fc1a0ec8ed5063e250a392368f4aa0
geniuslat <traber73@yandex.ru>
2026-07-25 03:14:18 +0400

    added Swagger

---
+++ vtmRevisedCharacterList/vtmRevisedCharacterListEntities/CubeRollRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace vtmRevisedCharacterListEntities;
+
+public class CubeRollRequest
+{
+    public uint CubesToRoll {  get; set; }
+
+    public uint Difficulty {  get; set; }
+
+    public string Comment { get; set; } = string.Empty;
+}
