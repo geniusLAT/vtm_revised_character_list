b814403fa3d7eb440567e7b8557fe756cb34c220
geniuslat <traber73@yandex.ru>
2026-08-24 16:12:50 +0400

    MeritEffect.cs

---
+++ vtmRevisedCharacterList/vtmRevisedCharacterList/AddFormHelper/MeritHelper.cs
@@ -0,0 +1,34 @@
+﻿using vtmRevisedCharacterListEntities;
+
+namespace vtmRevisedCharacterList.AddFormHelper;
+
+public class MeritHelper : IAddFormHelper
+{
+    public bool Rated => false;
+
+    public IEnumerable<string> GetAutoComplete()
+    {
+        List<string> result = [];
+        for (int i = 0; i < 83; i++)
+        {
+            var merit = (DefaultMerit)i;
+            result.Add(RussianTranslator.TranslateMerit(merit));
+        }
+        return result;
+    }
+
+    public void ProcessClick(AddARatingForm form, ARating item)
+    {
+        var index = form.FindClickedTextBoxIndex(item.Name);
+
+    }
+
+    public void ProcessCreatedItem(AddARatingForm form, ARating item)
+    {
+        var index = form.FindClickedTextBoxIndex(item.Name);
+
+        DefaultMerit defaultMerit = (DefaultMerit)index;
+        var merit = (MeritEntity)item;
+        merit.DefaultMerit = defaultMerit;
+    }   
+}
---
+++ vtmRevisedCharacterList/vtmRevisedCharacterListEntities/MeritEffect.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vtmRevisedCharacterListEntities;
+
+public class MeritEffect
+{
+    public int MeritDifficultyEffect { get; set; }
+
+    public int MeritDicepoolEffect { get; set; }
+
+    public bool DaredevilRemoveOne { get; set; }
+
+    public bool ExtraHealth { get; set; }
+
+    //Can not be activated, no mechanical effect. Only narrative
+    public bool Narrative { get
+        {
+            return MeritDifficultyEffect == 0 && MeritDicepoolEffect == 0 && !DaredevilRemoveOne && !ExtraHealth;
+        }
+    }
+}
