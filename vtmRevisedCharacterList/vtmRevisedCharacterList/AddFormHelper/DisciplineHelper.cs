8b55d5b2dce51fe7c6aca806361ae929611a0847
geniuslat <traber73@yandex.ru>
2026-08-24 13:46:46 +0400

    DisciplineHelper.cs

---
+++ vtmRevisedCharacterList/vtmRevisedCharacterList/AddFormHelper/DisciplineHelper.cs
@@ -0,0 +1,17 @@
+﻿using vtmRevisedCharacterListEntities;
+
+namespace vtmRevisedCharacterList.AddFormHelper;
+
+public class DisciplineHelper : IAddFormHelper
+{
+    public IEnumerable<string> GetAutoComplete()
+    {
+        List<string> result = [];
+        for (int i = 0; i < 24; i++)
+        {
+            var discipline = (DefaultDiscipline)i;
+            result.Add(RussianTranslator.TranslateDiscipline(discipline));
+        }
+        return result;
+    }
+}
