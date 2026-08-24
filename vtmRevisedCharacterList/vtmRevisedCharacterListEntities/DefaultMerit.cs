f64f66534bdc868cd3e8ea70f1d8dcdbaee66bcd
geniuslat <traber73@yandex.ru>
2026-08-24 14:59:24 +0400

    DefaultMerit enum was added

---
+++ vtmRevisedCharacterList/vtmRevisedCharacterListEntities/DefaultMerit.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vtmRevisedCharacterListEntities;
+
+public enum DefaultMerit
+{
+    #region Physical
+
+    AcuteSense = 0,
+    Ambidextrous = 1,
+    EatFood = 2,
+    CatlikeBalance = 3,
+    BlushOfHealth = 4,
+    EnchantingVoice = 5,
+    DareDevil = 6,
+    EfficientDigestion = 7,
+    HugeSize = 8,
+    SmellOfTheGrave = 9,
+    Short = 10,
+    HardOfHearing = 11,
+    FourteenthGeneration = 12,
+    InfectiousBite = 13,
+    BadSight = 14,
+    OneEye = 15,
+    Disfigured = 16,
+    Child = 17,
+    Deformity = 18,
+    Lame = 19,
+    Monstrous = 20,
+    PermanentWound = 21,
+    SlowHealing = 22,
+    Addiction = 23,
+    Mute = 24,
+    ThinBlood = 25,
+    DiseaseCarrier = 26,
+    Deaf = 27,
+    FleshOfTheCorpse = 28,
+    Blind = 29,
+
+    #endregion
+
+    #region Mental
+
+    CommonSense = 30,
+    Concentration = 31,
+    TimeSense = 32,
+    CodeOfHonor = 33,
+    EideticMemory = 34,
+    LightSleeper = 35,
+    NaturalLenguist = 36,
+    CalmHeart = 37,
+    IronWill = 38,
+    DeepSleeper = 39,
+    Nightmares = 40,
+    Phopia = 41,
+    PreyExclusion = 42,
+    Shy = 43,
+    SoftHearted = 44,
+    SpeechImpediment = 45,
+    ShortFuse = 46,
+    Territorial = 47,
+    Vengefil = 48,
+    Amnesia = 49,
+    Lunacy = 50,
+    WeakWilled = 51,
+    ConspicuousConsumption = 52,
+    #endregion
+
+    #region Social
+
+    PrestigiousSire = 53,
+    NaturalLeader = 54,
+    DebtOfGratitude = 55,
+    DarkSecret = 56,
+    InfamousSire = 57,
+    MistakenIdentity = 58,
+    SireResentiment = 59,
+    Enemy = 60,
+    Hunted = 61,
+    ProbationarySectMember = 62,
+    #endregion
+
+    #region Supernatural
+
+    Medium = 63,
+    MagicResistance = 64,
+    OracularAbility = 65,
+    SpiritMentor = 66,
+    Unbondable = 67,
+    Lucky = 68,
+    TrueLove = 69,
+    NineLives = 70,
+    TrueFaith = 71,
+    TouchOfFrost = 72,
+    RepulsedByGarlic = 73,
+    Cursed = 74,
+    CastNoReflection = 75,
+    EeriePresence = 76,
+    RepelledByCrosses = 77,
+    CantCrossRunningWater = 78,
+    Haunted = 79,
+    GripOfTheDamned = 80,
+    DarkFate = 81,
+    LightSensitive = 82,
+
+    #endregion
+}
