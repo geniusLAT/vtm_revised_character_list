e12b02408ea0fc016941fbd8caa5f00ba66bdca5
geniuslat <traber73@yandex.ru>
2026-08-24 12:16:10 +0400

    added DefaultDiscipline

---
+++ vtmRevisedCharacterList/vtmRevisedCharacterListEntities/DefaultDiscipline.cs
@@ -0,0 +1,46 @@
+﻿namespace vtmRevisedCharacterListEntities;
+
+public enum DefaultDiscipline
+{
+    Animalism = 0,
+
+    Auspex = 2,
+
+    Celerity = 3,
+
+    Chimerstry = 4,
+
+    Dementation = 5,
+
+    Dominate = 6,
+
+    Fortitude = 7,
+
+    Necromancy = 8,
+
+    Obfuscate = 9,
+
+    Obtenebration = 10,
+
+    Potence = 11,
+
+    Presence = 12,
+
+    Protean = 13,
+
+    Quietus = 14,
+
+    Serpentis = 15,
+
+    Vicissitude = 16,
+
+    ThaumaturgyPathOfBlood = 17,
+
+    ThaumaturgyLureOfFlames = 18,
+
+    ThaumaturgyMovementOfTheMind = 19,
+
+    ThaumaturgyPathOfConjuring = 20,
+
+    ThaumaturgyHandsOfDistraction = 21,
+}
